ff45a78631db1826ceb5994dd2972a406794b37e
Georgios Lymperopoulos <glympe@yahoo.gr>
2015-12-01 16:05:23 +0200

    Calculator working. More features to add

--- retro-calculator/ViewController.swift
+++ retro-calculator/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var outputLbl: UILabel!
     
+    enum Operation: String{
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        //case Equals = "="
+        case Empty = "Empty"
+    }
+    
+    @IBOutlet weak var outputLbl: UILabel!
+   
+    //Properties
     var btnSound = AVAudioPlayer!()
-
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var result = ""
+    var currentOperation: Operation = Operation.Empty
+    
+    
         override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,10 +48,74 @@ class ViewController: UIViewController {
         
     }
     
+    
+    
     @IBAction func numberPressed(btn: UIButton!){
-        btnSound.play()
-        
+        //playSound()
+        runningNumber += "\(btn.tag)"
+        outputLbl.text = runningNumber
     }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(Operation.Divide)
+    }
+    
+    @IBAction func onMultiply(sender: AnyObject) {
+        processOperation(Operation.Multiply)
+
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(Operation.Subtract)
+
+    }
+    
+    @IBAction func onAdditionPressed(sender: AnyObject) {
+        processOperation(Operation.Add)
+
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(currentOperation)
+
+    }
+    
+    func processOperation(op: Operation){
+        //playSound()
+        if currentOperation != Operation.Empty{
+            if runningNumber != ""{
+                rightValStr = runningNumber
+                runningNumber = ""
+                if currentOperation == Operation.Multiply{
+                    result = "\(Double(leftValStr)! * (Double(rightValStr))!)"
+                }else if currentOperation == Operation.Divide{
+                    result = "\(Double(leftValStr)! / (Double(rightValStr))!)"
+                }else if currentOperation == Operation.Subtract{
+                    result = "\(Double(leftValStr)! - (Double(rightValStr))!)"
+                }else if currentOperation == Operation.Add{
+                    result = "\(Double(leftValStr)! + (Double(rightValStr))!)"
+                }
+                leftValStr = result
+                outputLbl.text = result
+
+            }
+            currentOperation = op
+        }else{
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = op
+        }
+    }
+    
+    func playSound(){
+        if btnSound.play(){
+            btnSound.stop()
+        }
+        btnSound.play()
+    }
+    
+    
+    
 
     
     
